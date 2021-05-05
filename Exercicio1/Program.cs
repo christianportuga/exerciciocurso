c8caeeb34deec37d6b22d981dbbee0c3a2073c21
Christian Portugal <christianportuga@gmail.com>
2021-05-05 15:45:56 -0300

    Projeto Retangulo Pronto

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+            
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            x.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("AREA = " + x.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + x.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + x.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
